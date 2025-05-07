ed2a422fcf10f369f529d6eb789ef4eee6611e7c
limapedro12 <lima.pedro.0000@gmail.com>
2025-05-07 12:22:46 +0100

    Ordering games by date

--- fe/src/components/gameList/ShowGames.tsx
+++ fe/src/components/gameList/ShowGames.tsx
@@ -1,35 +1,49 @@
 // Create a new ShowGames component
 import React from 'react';
 
-const ShowGames = ({ games }) => (
-    <div className="games-section">
-        <h1>Os seus jogos</h1>
-        {games.map((game, index) => (
-            <div className="game-card" key={index}>
-                <div className="team">
-                    <div className="team-logo">Image</div>
-                    <div className="team-name">
-                        {game.home}
+const ShowGames = ({ games }) => {
+    const parseDate = (dateString: string, timeString: string): Date => {
+        const [day, month, year] = dateString.split('/').map(Number);
+        const [hours, minutes] = timeString.split(':').map(Number);
+        return new Date(year, month - 1, day, hours, minutes);
+    };
+
+    const sortedGames = games.sort((a, b) => {
+        const dateA = parseDate(a.date, a.time);
+        const dateB = parseDate(b.date, b.time);
+        return dateA - dateB;
+    });
+
+    return (
+        <div className="games-section">
+            <h1>Os seus jogos</h1>
+            {sortedGames.map((game, index) => (
+                <div className="game-card" key={index}>
+                    <div className="team">
+                        <div className="team-logo">Image</div>
+                        <div className="team-name">
+                            {game.home}
+                        </div>
+                    </div>
+                    <div className="game-info">
+                        <div>
+                            {game.date}
+                        </div>
+                        <div className="vs">VS</div>
+                        <div>
+                            {game.time}
+                        </div>
+                    </div>
+                    <div className="team">
+                        <div className="team-logo">Image</div>
+                        <div className="team-name">
+                            {game.away}
+                        </div>
                     </div>
                 </div>
-                <div className="game-info">
-                    <div>
-                        {game.date}
-                    </div>
-                    <div className="vs">VS</div>
-                    <div>
-                        {game.time}
-                    </div>
-                </div>
-                <div className="team">
-                    <div className="team-logo">Image</div>
-                    <div className="team-name">
-                        {game.away}
-                    </div>
-                </div>
-            </div>
-        ))}
-    </div>
-);
+            ))}
+        </div>
+    );
+};
 
 export default ShowGames;
