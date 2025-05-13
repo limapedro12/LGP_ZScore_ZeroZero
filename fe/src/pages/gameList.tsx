092986135fc52edb8ac159433d2e4bdd8452eb2a
limapedro12 <lima.pedro.0000@gmail.com>
2025-05-13 14:37:59 +0100

    Fix columns

--- fe/src/pages/gameList.tsx
+++ fe/src/pages/gameList.tsx
@@ -9,36 +9,11 @@ import ShowGames from '../components/gameList/ShowGames';
 
 const GameList = () => {
     const games = [
-        {
-            date: '22/03/2025',
-            time: '16:00',
-            home: 'Vitória SC',
-            away: 'Sporting CP',
-        },
-        {
-            date: '10/04/2025',
-            time: '14:00',
-            home: 'Leixões',
-            away: 'Sporting CP',
-        },
-        {
-            date: '22/03/2025',
-            time: '18:00',
-            home: 'Vitória SC',
-            away: 'Benfica',
-        },
-        {
-            date: '07/04/2025',
-            time: '20:00',
-            home: 'Vitória SC',
-            away: 'Leixões',
-        },
-        {
-            date: '16/04/2025',
-            time: '20:00',
-            home: 'Vitória SC',
-            away: 'Clube K',
-        },
+        { date: '22/03/2025', time: '16:00', home: 'Vitória SC', away: 'Sporting CP' },
+        { date: '10/04/2025', time: '14:00', home: 'Leixões', away: 'Sporting CP' },
+        { date: '22/03/2025', time: '18:00', home: 'Vitória SC', away: 'Benfica' },
+        { date: '07/04/2025', time: '20:00', home: 'Vitória SC', away: 'Leixões' },
+        { date: '16/04/2025', time: '20:00', home: 'Vitória SC', away: 'Clube K' },
     ];
 
     const [filteredGames, setFilteredGames] = useState(games);
@@ -48,26 +23,33 @@ const GameList = () => {
     };
 
     return (
-        <Container fluid className="scoreboard-container d-flex">
-            <Row>
-                <Col>
-                    {/* xs={5} md={5} lg={5}> */}
-                    <div className="logo">
-                        LOGO
+        <Container fluid className="scoreboard-container p-4">
+            {/* Top Section: Logo | Filters | Title */}
+            <Row className="mb-4 align-items-start">
+                <Col xs={4}>
+                    <div className="logo text-white">
+                        <strong>LOGO</strong>
                         <br />
-                        ZScore
+                        <span>ZScore</span>
                     </div>
                 </Col>
-                <Col>
-                    {/* xs={7} md={7} lg={7}> */}
-                    <h1>Os seus jogos</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={3} md={3} lg={3}>
+
+                <Col xs={4}>
                     <Filters games={games} onFilter={handleFilter} />
                 </Col>
-                <Col xs={5} md={5} lg={5}>
+
+            </Row>
+
+
+            {/* Game Display Section */}
+            <Row>
+                <Col xs={6}>
+                    <h1 className="mb-0 text-white jogosTitle">
+                        Os seus jogos
+                    </h1>
+                </Col>
+
+                <Col xs={{ span: 10, offset: 2 }}>
                     <ShowGames games={filteredGames} />
                 </Col>
             </Row>
