5b38050d0bfadc8df227b695f53471468acad374
Tomás Vicente <tasvicente@gmail.com>
2025-05-15 21:28:41 +0100

    linked game list to scorerTable view

--- fe/src/components/gameList/ShowGames.tsx
+++ fe/src/components/gameList/ShowGames.tsx
@@ -1,11 +1,16 @@
 // Create a new ShowGames component
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+// import { Button } from 'react-bootstrap';
+
 
 type Game = {
     home: string;
     away: string;
     date: string;
     time: string;
+    sport: string;
+    placardId: string;
 };
 
 type ShowGamesProps = {
@@ -13,6 +18,8 @@ type ShowGamesProps = {
 };
 
 const ShowGames: React.FC<ShowGamesProps> = ({ games }) => {
+    const navigate = useNavigate();
+
     const parseDate = (dateString: string, timeString: string): Date => {
         const [day, month, year] = dateString.split('/').map(Number);
         const [hours, minutes] = timeString.split(':').map(Number);
@@ -28,7 +35,14 @@ const ShowGames: React.FC<ShowGamesProps> = ({ games }) => {
     return (
         <div className="games-section">
             {sortedGames.map((game, index) => (
-                <div className="game-card" key={index}>
+                <div
+                    className="game-card"
+                    key={index}
+                    onClick={ () => {
+                        navigate(`../scorersTable/${game.sport}/${game.placardId}`);
+                    }
+                    }
+                >
                     <div className="team">
                         <div className="team-logo">Image</div>
                         <div className="team-name">
@@ -36,10 +50,11 @@ const ShowGames: React.FC<ShowGamesProps> = ({ games }) => {
                         </div>
                     </div>
                     <div className="game-info">
+                        <img src={`/icons/${game.sport}.png`} alt={`${game.sport} icon`} className="sport-icon" />
+                        <div className="vs">VS</div>
                         <div>
                             {game.date}
                         </div>
-                        <div className="vs">VS</div>
                         <div>
                             {game.time}
                         </div>
--- fe/src/pages/gameList.tsx
+++ fe/src/pages/gameList.tsx
@@ -12,15 +12,17 @@ type Game = {
     away: string;
     date: string;
     time: string;
+    sport: string;
+    placardId: string;
 };
 
 const GameList = () => {
     const games = [
-        { date: '22/03/2025', time: '16:00', home: 'Vitória SC', away: 'Sporting CP' },
-        { date: '10/04/2025', time: '14:00', home: 'Leixões', away: 'Sporting CP' },
-        { date: '22/03/2025', time: '18:00', home: 'Vitória SC', away: 'Benfica' },
-        { date: '07/04/2025', time: '20:00', home: 'Vitória SC', away: 'Leixões' },
-        { date: '16/04/2025', time: '20:00', home: 'Vitória SC', away: 'Clube K' },
+        { date: '22/03/2025', time: '16:00', home: 'Vitória SC', away: 'Sporting CP', sport: 'futsal', placardId: '1' },
+        { date: '10/04/2025', time: '14:00', home: 'Leixões', away: 'Sporting CP', sport: 'futsal', placardId: '2' },
+        { date: '22/03/2025', time: '18:00', home: 'Vitória SC', away: 'Benfica', sport: 'volleyball', placardId: '3' },
+        { date: '07/04/2025', time: '20:00', home: 'Vitória SC', away: 'Leixões', sport: 'volleyball', placardId: '4' },
+        { date: '16/04/2025', time: '20:00', home: 'Vitória SC', away: 'Clube K', sport: 'basketball', placardId: '5' },
     ];
 
     const [filteredGames, setFilteredGames] = useState(games);
